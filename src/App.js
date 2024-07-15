aa2597018cd16c31eb611d9ca061515d0a03adaa
robin-england <robin.england94@googlemail.com>
2024-07-15 17:56:22 -0400

    Implement tour fetching and rendering with state management.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 //U63166005
 // Coding Project #19
 
-import Gallery from "./Gallery";
+import Gallery from "./Gallery";  // imports Gallery
 
-export default function App() {
+export default function App() { // Runs App function, displaying Gallery
   return (
     <>
     <Gallery/>
--- src/Gallery.js
+++ src/Gallery.js
@@ -1,7 +1,44 @@
+import { useState } from "react";
+import { tourList } from "./tourList";
+
 export default function Gallery() {
+
+
+    const [tours, setTours] = useState(tourList.map(importedTours => { importedTours.display = false; return importedTours }))
+    // sets tours as the imported array for its initial state, adds display element to each object
+    const [showDetails, setShowDetails] = useState(false)   // initially hides details
+
+    function handleDetailsClick() {
+        setShowDetails(!showDetails)            // Reversed current state of show details
+    }
+
+
     return (
-        <>
-        Testing Gallery.js
-        </>
+        <div>
+
+            {tours.map(tour => (            // Maps and displays for each tour in the array
+                <div key={tour.id}>
+                    <h2>{tour.name}</h2>
+                    <img
+                        src={tour.image}
+                        alt={tour.name}
+                        width="350"
+                    />
+                    <br />
+                    <button onClick={handleDetailsClick}>{showDetails ? 'Show Less' : 'Read More'}</button>
+                    <button
+                        onClick={() => {
+                            setTours(
+                                tours.filter(i => i.id !== tour.id) // filters current item out of array
+                            )
+                        }}
+                    >Not Interested</button>
+                    <br />
+                    {showDetails && < b > ${tour.price}</b>}                    {/* Displays price and info only when showDetails is true */}
+                    <br />
+                    {showDetails && < p >{tour.info}</p>}
+                </div>
+            ))}
+        </div>
     )
 }
